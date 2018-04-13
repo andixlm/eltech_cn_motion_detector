fccc6c0fdc3c3b277c9e9dde6b17bf7d855cee63
andixlm <andixev@gmail.com>
2018-04-14 01:13:18 +0300

    app: Implement connection via TCP

--- SmartHomeThermometer/MainWindow.xaml.cs
+++ SmartHomeThermometer/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,11 +23,26 @@ namespace SmartHomeThermometer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly string IPADDRESS_LOG_LABEL = "IP Address: ";
+        private static readonly string PORT_LOG_LABEL = "Port: ";
+
+        private static readonly string CONNECTION_LOG_LABEL = "Connection: ";
+        private static readonly string CONNECTION_UP = "up";
+        private static readonly string CONNECTION_WAIT = "wait";
+        private static readonly string CONNECTION_DOWN = "down";
+        private static readonly string CONNECTION_ERR = "err";
+
         private static readonly string UPDATE_INTERVAL_LOG_LABEL = "Update interval: ";
 
         private Thermometer _Thermometer;
 
         private int _UpdateInterval;
+
+        private TcpClient _Socket;
+        private NetworkStream _SocketStream;
+
+        private IPAddress _IPAddress;
+        private int _Port;
 
         public MainWindow()
         {
@@ -40,6 +58,9 @@ namespace SmartHomeThermometer
 
             _UpdateInterval = Thermometer.DEFAULT_UPDATE_INTERVAL;
             UpdateIntervalTextBlock.Text = _UpdateInterval.ToString();
+
+            _Socket = new TcpClient();
+            _SocketStream = default(NetworkStream);
         }
 
         private void Configure()
@@ -51,6 +72,11 @@ namespace SmartHomeThermometer
             };
 
             /// Controls
+            ConnectButton.Click += (sender, e) =>
+            {
+                Connect();
+            };
+
             UpdateIntervalSetButton.Click += (sender, e) =>
             {
                 try
@@ -82,6 +108,71 @@ namespace SmartHomeThermometer
 
                 /// TODO: Send via socket to server.
             };
+        }
+
+        private void Connect()
+        {
+            ConnectButton.IsEnabled = !ConnectButton.IsEnabled;
+
+            try
+            {
+                _IPAddress = IPAddress.Parse(AddressTextBox.Text);
+            }
+            catch (Exception exc)
+            {
+                LogTextBlock.AppendText(IPADDRESS_LOG_LABEL + exc.Message);
+                ConnectButton.IsEnabled = !ConnectButton.IsEnabled;
+                return;
+            }
+
+            try
+            {
+                _Port = int.Parse(PortTextBox.Text);
+
+                if (_Port < 1024 || _Port > 49151)
+                {
+                    throw new Exception("Incorrect port value: [1024; 49151] ports are allowed");
+                }
+            }
+            catch (Exception exc)
+            {
+                LogTextBlock.AppendText(PORT_LOG_LABEL + exc.Message);
+                ConnectButton.IsEnabled = !ConnectButton.IsEnabled;
+                return;
+            }
+
+            Thread connectThread = new Thread(new ThreadStart(delegate ()
+            {
+                Dispatcher.Invoke(delegate ()
+                {
+                    LogTextBlock.AppendText(CONNECTION_LOG_LABEL +
+                    string.Format("Connecting to {0}:{1}\n", _IPAddress.ToString(), _Port));
+                    ThermometerConnectionValueLabel.Content = CONNECTION_WAIT;
+
+                    try
+                    {
+                        _Socket.Connect(_IPAddress, _Port);
+                        /// TODO: Get stream and send device info.
+
+                        LogTextBlock.AppendText(CONNECTION_LOG_LABEL +
+                            string.Format("Connected to {0}:{1}\n", _IPAddress.ToString(), _Port));
+                        ThermometerConnectionValueLabel.Content = CONNECTION_UP;
+                    }
+                    catch (SocketException exc)
+                    {
+                        LogTextBlock.AppendText(CONNECTION_LOG_LABEL + exc.Message + "\n");
+                        ThermometerConnectionValueLabel.Content = CONNECTION_ERR;
+                        ConnectButton.IsEnabled = !ConnectButton.IsEnabled;
+                    }
+                    catch (ObjectDisposedException exc)
+                    {
+                        LogTextBlock.AppendText(CONNECTION_LOG_LABEL + exc.Message + "\n");
+                        ThermometerConnectionValueLabel.Content = CONNECTION_DOWN;
+                        ConnectButton.IsEnabled = !ConnectButton.IsEnabled;
+                    }
+                });
+            }));
+            connectThread.Start();
         }
     }
 }
