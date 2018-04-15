02865d8a98017f5ff15da4fbf1b994a695f1dbfd
andixlm <andixev@gmail.com>
2018-04-15 17:42:39 +0300

    app: Carry out sending logic code

--- SmartHomeThermometer/MainWindow.xaml.cs
+++ SmartHomeThermometer/MainWindow.xaml.cs
@@ -185,13 +185,18 @@ namespace SmartHomeThermometer
             connectThread.Start();
         }
 
+        private void Send(byte[] data)
+        {
+            _SocketStream = _Socket.GetStream();
+            _SocketStream.Write(data, 0, data.Length);
+            _SocketStream.Flush();
+        }
+
         private void SendInfo()
         {
             byte[] data = Encoding.Unicode.GetBytes("Device: Thermometer");
 
-            _SocketStream = _Socket.GetStream();
-            _SocketStream.Write(data, 0, data.Length);
-            _SocketStream.Flush();
+            Send(data);
         }
     }
 }
